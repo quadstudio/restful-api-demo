9dff4987c12a6de67552d3da4607b8fc1bf89020
Alexander Shuleyko <salex009@gmail.com>
2020-02-02 13:36:11 +0300

    add 404 page design

--- resources/js/pages/NotFound.vue
+++ resources/js/pages/NotFound.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>404</div>
+    <div class="container">
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+            <div class="col-md-8 p-lg-8 mx-auto my-5">
+                <h1 class="display-4 font-weight-normal">404</h1>
+                <p class="lead font-weight-normal">Страница не найдена</p>
+                <router-link class="btn btn-outline-secondary" :to="{name: 'news.index'}">К новостям</router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
